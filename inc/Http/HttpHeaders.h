ffc8aa8d890b22ad779ecb336a0db05b5d9e3fa0
Fred Lawler <fred@fredlawl.com>
2016-09-08 19:23:38 -0500

    Added iterator to the headers

--- inc/Http/HttpHeaders.h
+++ inc/Http/HttpHeaders.h
@@ -10,11 +10,20 @@ namespace APIPOLLER {
 class APIPOLLER::HttpHeaders
 {
 public:
+    typedef settings_t::iterator iterator;
+    typedef settings_t::const_iterator const_iterator;
+
     inline const String& get(const String& headerKey) const;
     inline void insert(const String& headerKey, const String& headerValue);
     inline void remove(const String& headerKey);
 
     const string_array_t& getHeaderKeys() const;
+
+    inline iterator begin() { return headers.begin(); }
+    inline iterator end() { return headers.end(); }
+
+    inline const_iterator cbegin() { return headers.cbegin(); }
+    inline const_iterator cend() { return headers.cend(); }
 
     // @todo Need to add something for iteration ...
 
