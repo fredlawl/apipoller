9f2ae71b1fe36bbc4a9a488ada76d7cc175abfb4
Fred Lawler <fred@fredlawl.com>
2016-09-11 19:34:07 -0500

    Test poller can accept a HttpRequest

--- test/Pollers/TwitterPollerTest.cpp
+++ test/Pollers/TwitterPollerTest.cpp
@@ -1,5 +1,24 @@
-//
-// Created by Frederick Lawler on 9/11/16.
-//
+#include "gtest/gtest.h"
+#include "../Mocks/HttpRequestMock.h"
+#include "../../inc/Pollers/TwitterPoller.h"
 
-#include "TwitterPollerTest.h"
+class TwitterPollerTest : public ::testing::Test
+{
+protected:
+    HttpRequestMock* httpRequest = nullptr;
+
+    virtual void SetUp() {
+        httpRequest = new HttpRequestMock();
+    }
+
+    virtual void TearDown() {
+        delete httpRequest;
+    }
+};
+
+
+TEST_F(TwitterPollerTest, testTwitterPollerAcceptsHttpResponse)
+{
+    APIPOLLER::TwitterPoller poller(httpRequest);
+    ASSERT_TRUE(true);
+}
