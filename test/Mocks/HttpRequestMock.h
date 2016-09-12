35ed0d223ce812a101ee4d0553cb903cc6b5a1c0
Fred Lawler <fred@fredlawl.com>
2016-09-11 19:33:47 -0500

    Created a HttpRequestMock

--- test/Mocks/HttpRequestMock.h
+++ test/Mocks/HttpRequestMock.h
@@ -1,8 +1,26 @@
-//
-// Created by Frederick Lawler on 9/11/16.
-//
-
 #ifndef APIPOOLER_HTTPREQUESTMOCK_H
 #define APIPOOLER_HTTPREQUESTMOCK_H
 
-#endif //APIPOOLER_HTTPREQUESTMOCK_H
+#include "gmock/gmock.h"
+#include "../../inc/Http/HttpRequest.h"
+#include "../../inc/Http/HttpResponse.h"
+
+class HttpRequestMock : public APIPOLLER::HttpRequest
+{
+public:
+    HttpRequestMock() {}
+    MOCK_METHOD2(sendHeadersRequest, APIPOLLER::HttpResponse*(APIPOLLER::HttpRequest::Method method, const APIPOLLER::String& url));
+    MOCK_METHOD1(sendPostRequest, APIPOLLER::HttpResponse*(const APIPOLLER::String& url));
+    MOCK_METHOD1(sendGetRequest, APIPOLLER::HttpResponse*(const APIPOLLER::String& url));
+    MOCK_METHOD1(sendPutRequest, APIPOLLER::HttpResponse*(const APIPOLLER::String& url));
+    MOCK_METHOD1(sendDeleteRequest, APIPOLLER::HttpResponse*(const APIPOLLER::String& url));
+    MOCK_METHOD2(sendRequest, APIPOLLER::HttpResponse*(APIPOLLER::HttpRequest::Method method, const APIPOLLER::String& url));
+    MOCK_CONST_METHOD0(encodeParameters, APIPOLLER::settings_t*());
+    MOCK_CONST_METHOD0(buildQueryString, APIPOLLER::String());
+
+protected:
+    void open() {}
+    void close() {}
+};
+
+#endif
