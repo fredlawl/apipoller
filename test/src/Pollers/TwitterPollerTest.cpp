871a820fdc23823ed636f4e1058a3bc9406a1baa
Frederick Lawler <fred@40digits.com>
2016-09-12 20:56:44 -0500

    separated src and inc files in test directory

--- test/src/Pollers/TwitterPollerTest.cpp
+++ test/src/Pollers/TwitterPollerTest.cpp
@@ -1,5 +1,5 @@
 #include "gtest/gtest.h"
-#include "../Mocks/HttpRequestMock.h"
+#include "Mocks/HttpRequestMock.h"
 #include "Pollers/TwitterPoller.h"
 
 class TwitterPollerTest : public ::testing::Test
