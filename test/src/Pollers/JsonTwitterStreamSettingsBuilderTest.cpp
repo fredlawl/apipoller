2ebb32eebdce45bc69aa1445919fcfa19c92fb24
Frederick Lawler <fred@fredlawl.com>
2016-12-10 13:35:31 -0600

    added a test for testing the twitter settings for the twitter settings builder

--- test/src/Pollers/JsonTwitterStreamSettingsBuilderTest.cpp
+++ test/src/Pollers/JsonTwitterStreamSettingsBuilderTest.cpp
@@ -1,5 +1,6 @@
 #include <TwitterPoller/HttpStream/Configuration/TwitterHttpStreamSettings.h>
 #include <Http/Configuration/HttpRequestSettings.h>
+#include <TwitterPoller/Configuration/TwitterSettings.h>
 #include "gtest/gtest.h"
 #include "json/json.h"
 #include "Configuration/JsonConfigurationReader.h"
@@ -40,6 +41,26 @@ TEST_F(JsonTwitterStreamSettingsBuilderTest, testBuilder)
     settings_t settings;
     expected.requestConfiguration.parameters = settings;
     expected.requestConfiguration.parameters.insert(std::make_pair<String, String>("show_env", "1"));
+
+    delete builder;
+    delete actual;
+    builder = nullptr;
+}
+
+TEST_F(JsonTwitterStreamSettingsBuilderTest, testTwitterSettings)
+{
+    using namespace APIPOLLER;
+    Json::Value* config = configReader->loadFromFile("../test/files/testSettings.json");
+    builder = new TwitterStreamSettingsBuilder(config);
+
+    TwitterHttpStreamSettings* actual = builder->withTwitterSettings()->build();
+
+    TwitterHttpStreamSettings expected;
+    expected.twitter.consumerKey = "thisiskey";
+    expected.twitter.consumerSecret = "thisissecret";
+
+    ASSERT_STREQ(expected.twitter.consumerKey.c_str(), actual->twitter.consumerKey.c_str());
+    ASSERT_STREQ(expected.twitter.consumerSecret.c_str(), actual->twitter.consumerSecret.c_str());
 
     delete builder;
     delete actual;
